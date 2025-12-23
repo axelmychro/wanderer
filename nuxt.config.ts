25abf2ad9753e3c005c478b6f3e0eb3cbcee6587
Axel <axelmychro@gmail.com>
2025-12-23 09:30:01 +0700

    chore: set prettier to single quotes, disable trailing comma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxt/eslint",
-    "@nuxt/ui",
-    "@nuxt/hints",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/test-utils",
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
   ],
 
   devtools: {
-    enabled: true,
+    enabled: true
   },
 
-  css: ["~/assets/css/main.css"],
+  css: ['~/assets/css/main.css'],
 
   routeRules: {
-    "/": { prerender: true },
+    '/': { prerender: true }
   },
 
-  compatibilityDate: "2025-01-15",
+  compatibilityDate: '2025-01-15',
 
   nitro: {
-    preset: "netlify-edge",
+    preset: 'netlify-edge'
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: "never",
-        braceStyle: "1tbs",
-      },
-    },
-  },
-});
+        commaDangle: 'never',
+        braceStyle: '1tbs',
+        quotes: 'single'
+      }
+    }
+  }
+})
